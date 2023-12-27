56ec6afa69a84ff005eac110a331f0f3a9b88454
unknown <joshuajulius2030@gmail.com>
2023-12-27 12:14:14 +0100

    success

--- log/aboutUs.php
+++ log/aboutUs.php
@@ -3,25 +3,73 @@ require_once "../header.php";
 ?>
 <section>
     <div class="container">
-        <div class="row">
+        <div class="row bg-light">
             <div class="col-lg-6">
-                <img src="../img/logo5.png" class="img-fluid" alt="">
+                <img src="../img/watermark.png" class="img-fluid" alt="">
+                <p>
+                <h1 class="fw-bold text-center text-muted">About Us</h1>
+                </p>
             </div>
-            <div class="col-lg-6 mt-lg-5 text-dark fw-lighter fs-5">
+            <div class="col-lg-6 mt-lg-5 text-dark fw-lighter bg-light fs-5">
                 At veecla, we thrive to empower individuals to embrace their unique beauty and express themselves confidently
                 through our exceptional lip care, cosmetics, and fashion wear. We strive to provide high-quality products that
                 not only enhance your natural features but also inspire creativity and self-expression.
 
-                Ultimately, our mission is to be more than just a brand. We aspire to be a trusted companion, empowering
-                individuals to embrace their unique style, express themselves authentically, and feel confident in their own
-                skin. Join us on this exciting journey of self-discovery and let Veecla be your partner in unlocking your true
-                beauty potential.
                 <p>
-                    Our values:
-                    It was important to us to build a value driven business. At Veecla,we believe in: Simplicity. Affordability. Authenticity. Quality. Transparency.
+                    <br>
+                    Ultimately, our mission is to be more than just a brand. We aspire to be a trusted companion, empowering
+                    individuals to embrace their unique style, express themselves authentically, and feel confident in their own
+                    skin. Join us on this exciting journey of self-discovery and let Veecla be your partner in unlocking your true
+                    beauty potential.
                 </p>
 
+
             </div>
+        </div>
+        <div class="row fs-1 text-center mt-5">
+            <p>
+                Our goal is to provide a seamless and
+                straightforward experience that you can trust,
+                making your life easier and more enjoyable.
+                We're committed to delivering on our
+                promises and building lasting relationships
+                with our customers.
+            </p>
+        </div>
+
+        <div class="row bg-light">
+            <div class="col-4">
+                <img src="../img/aunty_vic.jpg" style="width:100%;" alt="">
+            </div>
+            <div class="col-4">
+                <img src="../img/aunty_vic.jpg" style="width:100%;" alt="">
+            </div>
+            <div class="col-4">
+                <img src="../img/aunty_vic.jpg" style="width:100%;" alt="">
+            </div>
+        </div>
+        <div class="row bg-light">
+            <div class="col-6">
+                <img src="../img/DSC_1705.jpg" style="width:100%;" alt="">
+            </div>
+            <div class="col-6">
+                <img src="../img/DSC_1705.jpg" style="width:100%;" alt="">
+            </div>
+
+        </div>
+        <div class="row text-center fs-1 mt-5">
+            <h1 class="fw-bold mb-3"> Our values</h1>
+            <p>
+
+                It was important to us to build a value driven business. At Veecla,we believe in: Simplicity. Affordability. Authenticity. Quality. Transparency.
+            </p>
+        </div>
+        <div class="row text-center fs-1 mt-5">
+            <h1 class="fw-bold mb-3"> Our Products are;</h1>
+            <p>
+
+                Cruelty-Free
+            </p>
         </div>
     </div>
     <?php
