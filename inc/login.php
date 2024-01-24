18cd1199f681e0c78c5d0715fa0f3fdb1298497f
unknown <joshuajulius2030@gmail.com>
2024-01-24 19:39:05 +0100

    success

--- inc/login.php
+++ inc/login.php
@@ -81,6 +81,7 @@ require_once __DIR__ . "/../config/session.php";
 
             </div>
             <div class="content">
+                <!-- displays an alert when user signup in successfully -->
                 <div class="alert"></div>
                 <div class="title">
                     Login your Account to get access
